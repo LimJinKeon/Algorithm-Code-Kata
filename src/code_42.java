909e2670e67e4be2f4635aa47d548a502f25a724
Lim JinKeon <0907john24@gmail.com>
2026-02-10 10:15:53 +0900

    이상한 수 만들기

---
+++ src/code_42.java
@@ -0,0 +1,28 @@
+public class code_42 {
+    public static String solution(String s) {
+        String answer = "";
+        int idx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ') {
+                answer += c;
+                idx = 0;
+            } else {
+                if (idx % 2 == 0) {
+                    answer += Character.toUpperCase(c);
+                } else {
+                    answer += Character.toLowerCase(c);
+                }
+                idx++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "try hello world";
+        System.out.println(solution(s));
+    }
+}
