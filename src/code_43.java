e5d083076a9873c9be37c83de507e33f972a2ae9
Lim JinKeon <0907john24@gmail.com>
2026-02-10 10:25:10 +0900

    삼총사

---
+++ src/code_43.java
@@ -0,0 +1,19 @@
+public class code_43 {
+    public static int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.length - 2; i++){
+            for(int j = i + 1; j < number.length - 1; j++){
+                for(int k = j + 1; k < number.length; k++){
+                    if(number[i] + number[j] + number[k] == 0)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 3, 0, 2, -5};
+        System.out.println(solution(arr));
+    }
+}
