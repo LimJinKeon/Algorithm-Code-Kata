f8a4ea546e3193601d331f785366773c411bf1bd
Lim JinKeon <0907john24@gmail.com>
2026-02-20 10:37:54 +0900

    JadenCase 문자열 만들기

---
+++ src/level4/code_76.java
@@ -0,0 +1,30 @@
+package level4;
+
+public class code_76 {
+    public static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        boolean isFirst = true;
+
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                answer.append(" ");
+                isFirst = true;
+            }
+            else {
+                if (isFirst) {
+                    answer.append(Character.toUpperCase(s.charAt(i)));
+                    isFirst = false;
+                }
+                else {
+                    answer.append(Character.toLowerCase(s.charAt(i)));
+                }
+            }
+        }
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "3people unFollowed me";
+        System.out.println(solution(s));
+    }
+}
