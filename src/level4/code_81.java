f69c8bd64c645710584ce189bad08d3a777d5d17
Lim JinKeon <0907john24@gmail.com>
2026-03-04 10:12:04 +0900

    N개의 최소공배수

---
+++ src/level4/code_81.java
@@ -0,0 +1,35 @@
+package level4;
+
+public class code_81 {
+    public static int solution(int[] arr) {
+        if(arr.length == 1) return arr[0];
+
+        int answer = lcm(arr[0], arr[1]);
+
+        for(int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+
+        return answer;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static int gcd(int a, int b) {
+        if(a % b == 0)
+            return b;
+        else
+            return gcd(b, a % b);
+    }
+
+    private static int next(int x) {
+        return (x % 2 == 0) ? x / 2 : ((x / 2) + (x % 2));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 8, 14};
+        System.out.println(solution(arr));
+    }
+}
